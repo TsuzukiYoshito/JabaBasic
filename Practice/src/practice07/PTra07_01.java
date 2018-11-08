462884bef4a47437f5adb9b1dedd8cb52455c4dc
TsuzukiYoshito <44760278+TsuzukiYoshito@users.noreply.github.com>
2018-11-08 16:25:53 +0900

    8-1まで終わりました

--- Practice/src/practice07/PTra07_01.java
+++ Practice/src/practice07/PTra07_01.java
@@ -10,15 +10,28 @@ public class PTra07_01 {
 	public static void main(String[] args) {
 
 		// ★ int型の配列arrayを宣言してください
-
+        int [] score;
 
 		// ★ 変数arrayの配列を5個の領域で確保してください
-
+         score=new int[5];
 
 		// ★ 変数arrayにそれぞれ、[1,4,7,10,13] を代入してください
-
+          
+         score[0]=1;
+         score[1]=4;
+         score[2]=7;
+         score[3]=10;
+         score[4]=13; 
+         
+         
+         
 
 		// ★ 変数arrayに格納されている値を前から順番に全て表示してください
-
+            for(int value:score) {
+            	System.out.println(value)
+            	;
+            }
+         
+         
 	}
 }
