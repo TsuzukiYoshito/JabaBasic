af9a225050aacd1f363ca87338f8defa18322f90
TsuzukiYoshito <44760278+TsuzukiYoshito@users.noreply.github.com>
2018-11-08 18:01:49 +0900

    8-3まで終わりました

--- Practice/src/practice08/PTra08_02.java
+++ Practice/src/practice08/PTra08_02.java
@@ -13,18 +13,18 @@ public class PTra08_02 {
 		int y = 50;
 
 		// ★ 変数receiveを宣言して、averageメソッドを（変数x, 変数y）で呼び出した結果を代入してください
-
+          String receive= average( x ,y);
+           
+          
 
 		// ★ 変数receiveを出力してください
-
+            System.out.println(receive);
 	}
 
 	/*
 	 * 以下の仕様でメソッドを作成しました
 	 *
-	 * 戻り値		：String
-	 * メソッド名	：average
-	 * 引数			：int x, int y
+	 * 戻り値 ：String メソッド名 ：average 引数 ：int x, int y
 	 *
 	 * 引数で渡された２つのint型の平均値を返すメソッド
 	 */
