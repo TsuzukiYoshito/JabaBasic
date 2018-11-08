f8bd3b3dce412893f8a5923aa5509823cb494658
TsuzukiYoshito <44760278+TsuzukiYoshito@users.noreply.github.com>
2018-11-08 10:46:20 +0900

    六の七

--- Practice/src/practice06/PTra06_06.java
+++ Practice/src/practice06/PTra06_06.java
@@ -7,6 +7,7 @@ package practice06;
  */
 
 public class PTra06_06 {
+	
 	public static void main(String[] args) {
 
 		// ★ for文を使って、以下のような出力になるようにしてください
@@ -19,6 +20,43 @@ public class PTra06_06 {
 		 * ■■■□□
 		 * ■■■■□
 		 */
-
+         
+         for(int i=1;i<6;i++){
+         
+        		System.out.print("□");
+         }
+        System.out.println();	
+         
+	
+        {
+         for(int i=1;i<2;i++)
+        System.out.print("■");
+        
+         System.out.print("□"+"□"+"□"+"□");
+         
+       
+         System.out.println();	
+        for(int i=1;i<3;i++)
+            System.out.print("■");
+         
+        System.out.print("□"+"□"+"□");
+         
+        
+        System.out.println();	
+        for(int i=1;i<4;i++)
+            System.out.print("■");
+            
+        System.out.print("□"+"□");
+         
+        System.out.println();	
+        for(int i=1;i<5;i++)
+            System.out.print("■");
+            
+        System.out.print("□");
+        
+         }	
 	}
 }
+
+
+
--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -25,5 +25,76 @@ public class PTra06_07 {
 		 * ■□□□□
 		 */
 
+		 for(int i=1;i<6;i++){
+	         
+     		System.out.print("□");
+      }
+     System.out.println();	
+      
+	
+     {
+      for(int i=1;i<2;i++)
+     System.out.print("■");
+     
+      System.out.print("□"+"□"+"□"+"□");
+      
+    
+      System.out.println();	
+     for(int i=1;i<3;i++)
+         System.out.print("■");
+      
+     System.out.print("□"+"□"+"□");
+      
+     
+     System.out.println();	
+     for(int i=1;i<4;i++)
+         System.out.print("■");
+         
+     System.out.print("□"+"□");
+      
+     System.out.println();	
+     for(int i=1;i<5;i++)
+         System.out.print("■");
+         
+     System.out.print("□");
+     
+     System.out.println();	
+     for(int i=1;i<6;i++)
+         System.out.print("■");
+         
+     
+     System.out.println();	
+     for(int i=1;i<5;i++)
+         System.out.print("■");
+     System.out.print("□");
+     
+     System.out.println();	
+     
+     for(int i=1;i<4;i++)
+         System.out.print("■");
+     System.out.print("□"+"□");
+     
+     System.out.println();	
+     for(int i=1;i<3;i++)
+         System.out.print("■");
+      
+     System.out.print("□"+"□"+"□");
+      
+     System.out.println();
+     for(int i=1;i<2;i++)
+         System.out.print("■");
+         
+          System.out.print("□"+"□"+"□"+"□");
+     
+     
+      }	
 	}
 }
+		
+		
+		
+		
+		
+		
+	
+
