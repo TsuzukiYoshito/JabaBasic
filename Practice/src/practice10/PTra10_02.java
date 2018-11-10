44648ba61cb60b7306f3f996c71ae41ef8659d0b
TsuzukiYoshito <44760278+TsuzukiYoshito@users.noreply.github.com>
2018-11-10 20:29:30 +0900

    10-2

--- Practice/src/practice10/PTra10_02.java
+++ Practice/src/practice10/PTra10_02.java
@@ -16,8 +16,12 @@ public class PTra10_02 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
-
-
+		 User us=new User();
+		    us.userId=101;
+		    us.userNm="リゾーム太郎";
+		    us.mail="rhizome@gamil.com";		
+		   us.password="1qazxcv";
+           
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
 
 
@@ -34,6 +38,14 @@ public class PTra10_02 {
 
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		   System.out.println(us.userId);
+   		System.out.println(us.userNm);
+   		System.out.println(us.mail);
+   		System.out.println(us.password);
+		   
+		   
+		   
+		   
+		   
 	}
 }
