5ac97fc3f9623ae826130c900110e27a8545c269
TsuzukiYoshito <44760278+TsuzukiYoshito@users.noreply.github.com>
2018-11-21 13:57:51 +0900

    13-7

--- Practice/src/practice13/common/Item.java
+++ Practice/src/practice13/common/Item.java
@@ -1,8 +1,15 @@
+
 package practice13.common;
 
 public class Item {
+
 	private String name;
 	private int additionalDamage;
+
+	public Item(String name, int additionalDamage) {
+		this.name = name;
+		this.additionalDamage = additionalDamage;
+	}
 
 	public String getname() {
 		return name;
@@ -10,15 +17,14 @@ public class Item {
 
 	public void setname(String name) {
 		this.name = name;
-
 	}
+
 	public int getadditionalDamage() {
 		return additionalDamage;
 	}
 
-	public void setadditionalDamage(int additionalDamage ) {
-	     this.additionalDamage=additionalDamage;
-
-
+	public void setadditionalDamage(int additionalDamage) {
+		this.additionalDamage = additionalDamage;
 	}
+
 }
--- Practice/src/practice13/common/SuperHero.java
+++ Practice/src/practice13/common/SuperHero.java
@@ -1,13 +1,36 @@
 package practice13.common;
+/*
+ * ★ common.Heroクラスを継承した、common.SuperHeroクラスを作成してください
+ * 	●HP, POWER, ENDURANCEはそれぞれ、（25, 10, 7）
+ *
+ * フィールド
+ * 		equipment	:	common.Item
+ * メソッド
+ * 		attackメソッドをオーバーライドし、
+ * 		powerに加えて、additionalDamage分を加算した値としてください
+ *
+ * 		equipmentのアクセサ
+ */
+public class SuperHero extends Hero{
 
-public class SuperHero extends Hero {
-    public SuperHero() {
-      super(25,10,7);
 
 
+	private Item  equipment;
 
 
+	public Item getEquipment() {
+		return equipment;
+	}
 
 
-}
+	public void setEquipment(Item equipment) {
+		this.equipment = equipment;
+	}
+
+
+	public int attack(){
+		return super.attack() + equipment.getadditionalDamage();
+
+	}
+
 }
