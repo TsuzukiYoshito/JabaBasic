e36e176f22826462bdfa422742aa68a5e643402f
TsuzukiYoshito <44760278+TsuzukiYoshito@users.noreply.github.com>
2018-12-04 10:07:40 +0900

    18-3

--- Practice/src/practice18/PTra18_03.java
+++ Practice/src/practice18/PTra18_03.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_03 {
 
 	/*
@@ -15,17 +23,87 @@ public class PTra18_03 {
 	public static void main(String[] args) {
 
 		/*
-		 * ①
-		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
-		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
+		 * ① ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください ★
+		 * ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		ArrayList<Player> array = new ArrayList<>();
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+			while (scanner.hasNext()) {
+
+				// ここでplayerインスタンスを作ること
+				Player player1 = new Player();
+
+				String box1 = scanner.nextLine();
+
+				String[] box2 = box1.split(",", 0);
+
+				player1.setposition(box2[0]);
+				player1.setname(box2[1]);
+				player1.setcountry(box2[2]);
+				player1.setteam(box2[3]);
+
+				array.add(player1);
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりません");
+		}
 
 
-		// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
 
 
-		// ★ 削除後のArrayListの中身を全件出力してください
+		List<Player> set = new ArrayList<>();
 
 
+		for(int i=0;i<array.size();i++) {
+			if(array.get(i).getteam().equals("バルセロナ")) {
+				set.add(array.get(i));
+			}
+		}
+
+		for(int i=0;i<array.size();i++) {
+			if(array.get(i).getteam().equals("レアル・マドリード")) {
+				set.add(array.get(i));
+
+
+			}
+		}
+
+
+
+
+		// b,c,dをlist2から削除する
+		array.removeAll(set);
+
+
+
+		for (Player name : array) {
+			System.out.println(name);
+
+		}
 	}
+
+	// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
+
+	// ★ 削除後のArrayListの中身を全件出力してください
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,15 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -15,13 +24,96 @@ public class PTra18_04 {
 	public static void main(String[] args) {
 
 		/*
-		 * ①
-		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
-		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
+		 * ① ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください ★
+		 * ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		ArrayList<Player> array = new ArrayList<>();
+
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+			while (scanner.hasNext()) {
+
+				// ここでplayerインスタンスを作ること
+				Player player1 = new Player();
+
+				String box1 = scanner.nextLine();
+				String[] box2 = box1.split(",", 0);
+
+				player1.setposition(box2[0]);
+				player1.setname(box2[1]);
+				player1.setcountry(box2[2]);
+				player1.setteam(box2[3]);
+
+				array.add(player1);
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりません");
+		}
+
+		Collections.shuffle(array);
 
 
-		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+
+		List<Player> set = new ArrayList<>();
+		List<Player> set1 = new ArrayList<>();
+		List<Player> set2 = new ArrayList<>();
+		List<Player> set3 = new ArrayList<>();
+
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i).getposition().equals("GK")) {
+				set.add(array.get(i));
+			}
+		}
+
+		for (int h = 0; h <array.size(); h++) {
+			if (array.get(h).getposition().equals("DF")) {
+				set1.add(array.get(h));
+
+			}
+		}
+
+		for (int j = 0; j < array.size(); j++) {
+			if (array.get(j).getposition().equals("MF")) {
+				set2.add(array.get(j));
+
+			}
+		}
+
+		for (int k = 0; k < array.size(); k++) {
+			if (array.get(k).getposition().equals("FW")) {
+				set3.add(array.get(k));
+
+			}
+		}
+
+
+
+
+
+
+
+
+		for(int l=0;l<1;l=l++){
+            System.out.println(set.get(l));
+
+		}
+
+		for(int l=0;l<4;l=l++){
+            System.out.println(set1.get(l));
+
+		}
+		for(int l=0;l<4;l=l++){
+            System.out.println(set2.get(l));
+		}
+		for(int l=0;l<2;l=l++){
+            System.out.println(set3.get(l));
+
+		}
+				}
+
+				// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+
+
+
 
 	}
-}
